1b087925dc4f4b95c7cc3ab1bbdf8ce86b9130c1
guru7520k <guruprakash848@gmail.com>
2021-06-29 10:07:20 +0530

    commiting html

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,19 @@
-<h1>Test</h1>
+<script>
+
+import Timer from "./Timer.svelte";
+import HowTo from "./HowTo.svelte";
+
+</script>
+<style>
+    h1{
+        text-align: center;
+    }
+
+
+</style>
+
+
+<h1>Hand Washing-App </h1>
+
+<Timer />
+<HowTo />
---
+++ src/HowTo.svelte
@@ -0,0 +1,11 @@
+<style>
+    img{
+        max-width: 100%;
+    }
+</style>
+
+
+
+<div bp="offset-5@md 4@md 12@sm">
+    <img src="image1.png" alt="How to wash your hands.">
+</div>
